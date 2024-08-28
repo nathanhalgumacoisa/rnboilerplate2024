57fbc004cda741643f503150becdd88ca7f906f6
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2024-08-28 12:57:02 -0300

    feat: create screens navigation

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Home() {
+  const navigation = useNavigation();
   return (
     <View>
       <Text>Home screen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("Totoro")}>
+        <Text>Go to Totoro</Text>
+      </TouchableOpacity>
     </View>
   )
 }
--- src/screens/Totoro/index.jsx
+++ src/screens/Totoro/index.jsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Totoro() {
+  const Navigation = useNavigation()
   return (
     <View>
       <Text>My neighbor Totoro</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+        <Text>Back to home</Text>
+      </TouchableOpacity>
     </View>
   )
 }
